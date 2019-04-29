eb6e0080aa09656e63a0731937bebfc3ed630af5
Wioletta Mańka <wioletta.manka728@gmail.com>
2019-04-29 23:23:50 +0200

    Admin Page - Manage Ingredients Categories

--- CookBookApp/Controllers/IngredientsCategoriesController.cs
+++ CookBookApp/Controllers/IngredientsCategoriesController.cs
@@ -3,20 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CookBookApp.Data;
+using CookBookApp.DTO;
+using CookBookApp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CookBookApp.Controllers
 {
     [Route("api/ingredientscategories")]
-    [ApiController]
-    public class IngredientsCategoriesController : ControllerBase
+    public class IngredientsCategoriesController : Controller
     {
         private readonly ApplicationDbContext context;
 
         public IngredientsCategoriesController(ApplicationDbContext context) => this.context = context;
 
         [HttpGet]
+        // GET: api/ingredientscategories
         public IActionResult Get()
         {
             var ingredientsCategories = context.IngredientCategory.OrderByDescending(i => i.Name).ToList();
@@ -25,7 +27,7 @@ namespace CookBookApp.Controllers
         }
 
         // GET api/ingredientscategories/{id}
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetIngredientCategory")]
         public IActionResult Get(int id)
         {
             var result = context.IngredientCategory.FirstOrDefault(ic => ic.Id == id);
@@ -33,6 +35,38 @@ namespace CookBookApp.Controllers
             if (result == null) return NotFound();
 
             return Ok(result);
+        }
+
+        // POST api/ingredientscategories
+        [HttpPost]
+        public IActionResult Post(IngredientCategoryDTO ingredientCategory)
+        {
+            if (ingredientCategory == null) return BadRequest();
+
+            var finalCategory = new IngredientCategory()
+            {
+                Name = ingredientCategory.Name
+            };
+
+            context.IngredientCategory.Add(finalCategory);
+            context.SaveChanges();
+
+            return CreatedAtRoute("GetIngredientCategory", new { id = finalCategory.Id }, finalCategory);
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var category = context.IngredientCategory.FirstOrDefault(ic => ic.Id == id);
+
+            if (category == null)
+                return NotFound();
+
+            context.IngredientCategory.Remove(category);
+            context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
---
+++ CookBookApp/DTO/IngredientCategoryDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookBookApp.DTO
+{
+    public class IngredientCategoryDTO
+    {
+        public string Name { get; set; }
+    }
+}
